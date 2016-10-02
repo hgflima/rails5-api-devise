7284fd3f4ea9b627189bcb779d98493e4e0451a1
Henrique Lima <hgflima@gmail.com>
2016-10-02 00:38:54 -0300

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :confirmable
+
+  include DeviseTokenAuth::Concerns::User
+
+end
